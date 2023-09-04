ec84073f7e7ebda6dc126cbde47c0ca0a47bc041
yoshi47 <yoshix47@gmail.com>
2023-09-04 01:50:35 +0000

    abc185 a

---
+++ abc185/src/bin/a.rs
@@ -0,0 +1,14 @@
+use proconio::input;
+
+fn main() {
+    input! {
+        a: [i32; 4],
+    }
+    let mut ans = 100;
+    for &i in &a {
+        if ans > i {
+            ans = i;
+        }
+    }
+    println!("{}", ans);
+}
