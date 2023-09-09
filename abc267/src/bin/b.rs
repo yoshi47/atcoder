d2af6ef40bc87d4e993f99ed453a91d70cb61adc
yoshi47 <yoshix47@gmail.com>
2023-09-09 01:22:17 +0000

    abc267 b wa

--- abc267/src/bin/b.rs
+++ abc267/src/bin/b.rs
@@ -1,5 +1,16 @@
 use proconio::input;
+use proconio::marker::Chars;
 
 fn main() {
-    todo!();
+    input! {
+        a: Chars,
+    }
+    
+    let d: [i32; 10] = [4, 3, 5, 2, 4, 6, 1, 3, 5, 7];
+
+    for i in 1..11 {
+        if a[i].to_string() == "1" {
+            
+        }
+    }
 }
