72d37f0cfba234a35f98c7ba45fdd7204a37b012
yoshi47 <yoshix47@gmail.com>
2023-11-10 06:56:44 +0000

    まよコン 振り返り

--- abc225/src/bin/c.rs
+++ abc225/src/bin/c.rs
@@ -6,30 +6,25 @@ fn main() {
         m: usize,
         b: [[usize; m]; n],
     };
+
     for i in 0..n {
-        if b[i][0..m-1].iter().all(|&x| x % 7 == 0) {
+        if b[i][0..m-1].iter().any(|&x| x % 7 == 0) {
             println!("No");
             return;
         }
-        // let row_s: usize = b[i].iter().sum();
-        // let t_sum = m * (b[i][0] + b[i][m-1]) / 2;
-        // if !(row_s == t_sum) {
-        //     println!("No");
-        //     return;
-        // }
 
+        let tmp: Vec<usize> = (b[i][0]..b[i][0]+m).collect();
+        if !(b[i] == tmp) {
+            println!("No");
+            return;
+        }
+        
         if !(i == n-1) {
-            for j in 0..m {
-                if !(b[i][j] + 7 == b[i+1][j]) {
-                    println!("No");
-                    return;
-                }
+            let tmp: Vec<usize> = b[i].iter().map(|&x| x + 7).collect();
+            if !(b[i+1] == tmp) {
+                println!("No");
+                return;
             }
-            // if !(b[i][0] + 7 == b[i+1][0]) {
-            //     println!("No");
-            //     return;
-            // }
-
         }
     }
     println!("Yes");
--- abc308/src/bin/c.rs
+++ abc308/src/bin/c.rs
@@ -1,18 +1,32 @@
+use itertools::Itertools;
 use proconio::input;
+
+// fn main() {
+//     input! {
+//         n: usize,
+//         p: [(f64, f64); n],
+//     }
+//     let mut r = vec![(0_usize, 0_f64); n];
+//     for i in 0..n {
+//         r[i] = (i+1, p[i].0 / (p[i].0 + p[i].1));
+//     }
+//     r.sort_by(|a, b| b.1.partial_cmp(&a.1).unwrap());
+//     // r.sort_by(|a, b| (-a.1).partial_cmp(&(-b.1)).unwrap());
+//     for i in 0..n {
+//         print!("{} ", r[i].0);
+//     }
+
+// }
 
 fn main() {
     input! {
         n: usize,
-        p: [(f64, f64); n],
+        ab: [(usize, usize); n],
     }
-    let mut r = vec![(0_usize, 0_f64); n];
-    for i in 0..n {
-        r[i] = (i+1, p[i].0 / (p[i].0 + p[i].1));
-    }
-    r.sort_by(|a, b| b.1.partial_cmp(&a.1).unwrap());
-    // r.sort_by(|a, b| (-a.1).partial_cmp(&(-b.1)).unwrap());
-    for i in 0..n {
-        print!("{} ", r[i].0);
-    }
+    let mut p: Vec<(usize, &(usize, usize))> = ab.iter().enumerate().collect();
+    p.sort_by(|(_, a), (_, b)| {
+        (b.0 * (a.0 + a.1)).partial_cmp(&(a.0 * (b.0 + b.1))).unwrap()
+    });
 
+    println!("{}", p.iter().map(|(i, _)| (i+1)).join(" "));
 }
