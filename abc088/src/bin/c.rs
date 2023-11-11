5d7100fd59a0a1981891f009341fd23cb61956b1
yoshi47 <yoshix47@gmail.com>
2023-11-11 15:41:03 +0900

    まよコン 振り返り

--- abc088/src/bin/c.rs
+++ abc088/src/bin/c.rs
@@ -1,14 +1,28 @@
 use proconio::input;
-
 fn main() {
     input! {
-        x[[usize]],
+        c: [[isize; 3]; 3],
     }
-    let mut (a1, a2, a3) = (0, 0, 0);
-    let mut (b1, b2, b3) = (0, 0, 0);
-    while a1 + b1 <= x[0][0] {
-        while a1 <= b1 {
-            
+
+    for i in 0..c[0][0]+1 {
+        let a1 = i;
+        let b1 = c[0][0] - i;
+        if b1 < 0 {continue};
+
+        let mut a2 = 0;
+        let mut b2 = 0;
+        if (c[1][0] - b1) + (c[0][1] - a1) == c[1][1] {
+            a2 = c[1][0] - b1;
+            b2 = c[0][1] - a1;
+        } else { continue }
+
+        if (c[2][0] - b1) == (c[2][1] - b2) &&
+            (c[0][2] - a1) == (c[1][2] - a2) &&
+            (c[2][0] -b1) + (c[0][2] - a1) == c[2][2] {
+            println!("Yes");
+            return;
         }
     }
+    println!("No");
+    
 }
